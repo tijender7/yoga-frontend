4128994036c4c7756bcdf7514649c013eb13944d
tijender7 <tijender.singh7@gmail.com>
2024-11-10 13:03:10 +0100

    fix: API URL configuration and routes

--- config.ts
+++ config.ts
@@ -5,4 +5,7 @@ if (!API_URL) {
   throw new Error('NEXT_PUBLIC_API_URL environment variable is not set');
 }
 
-export default API_URL;
+export const API_ROUTES = {
+  SIGNUP: `${API_URL}/auth/signup`,
+  FREE_CLASS: `${API_URL}/auth/free-class`,
+}
