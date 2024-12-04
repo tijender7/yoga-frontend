955d5c470abe675009c34e75803ee7f601f2911a
tijender7 <tijender.singh7@gmail.com>
2024-12-04 12:42:58 +0100

    book free class fixed

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -28,13 +28,13 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 text-sm font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-start md:items-center justify-between py-4 text-sm font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180 text-left pr-8 relative",
         className
       )}
       {...props}
     >
-      {children}
-      <ChevronDownIcon className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      <span className="mr-4">{children}</span>
+      <ChevronDownIcon className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200 absolute right-0 top-5" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
